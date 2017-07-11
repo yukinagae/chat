e5b2eea8bad82d2d7b4ec9ff50b61fd63a8eec6a
Yuki Nagae <yuki.nagae1130@gmail.com>
2017-07-11 18:04:24 +1000

    use Evented trait instead Handler (deprecated)
    see: https://github.com/carllerche/mio/issues/360

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate mio;
 
 use mio::*;
-use mio::tcp::{TcpListener};
+use mio::tcp::TcpStream;
 
 use std::net::SocketAddr;
+use std::io;
 
 const SERVER: Token = Token(0);
 
@@ -13,10 +14,12 @@ fn main() {
     // event_loop.run(&mut handler);
 
     let address = "0.0.0.0:10000".parse::<SocketAddr>().unwrap();
-    let server_socket = TcpListener::bind(&address).unwrap();
+    let server_socket = TcpStream::connect(&address).unwrap();
+
+    let server = WebSocketServer { socket: server_socket };
 
     poll.register(
-        &server_socket,
+        &server,
         SERVER,
         Ready::readable(),
         PollOpt::edge()
@@ -30,7 +33,7 @@ fn main() {
         for event in events.iter() {
             match event.token() {
                 SERVER => {
-                    let _ = server_socket.accept();
+                    // TODO: do something
                 }
                 _ => unreachable!(),
             }
@@ -38,9 +41,24 @@ fn main() {
     }
 }
 
-// struct WebSocketServer;
+struct WebSocketServer {
+    socket: TcpStream,
+}
 
-// impl Handler for WebSocketServer {
-//     type Timeout = usize;
-//     type Message = ();
-// }
+impl Evented for WebSocketServer {
+
+    fn register(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt) -> io::Result<()> {
+        // Delegate the `register` call to `socket`
+        self.socket.register(poll, token, interest, opts)
+    }
+
+    fn reregister(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt) -> io::Result<()> {
+        // Delegate the `reregister` call to `socket`
+        self.socket.reregister(poll, token, interest, opts)
+    }
+
+    fn deregister(&self, poll: &Poll) -> io::Result<()> {
+        // Delegate the `deregister` call to `socket`
+        self.socket.deregister(poll)
+    }
+}
