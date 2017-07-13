e1b3d0c57bce2ecb8845913fb56e1fde0ab8aa3c
Yuki Nagae <yuki.nagae1130@gmail.com>
2017-07-13 18:50:46 +1000

    ws connection established

--- src/main.rs
+++ src/main.rs
@@ -48,38 +48,58 @@ fn main() {
         poll.poll(&mut events, None).unwrap();
 
         for event in events.iter() {
-            match event.token() {
-                // first connection
-                SERVER => {
-                    let client_socket = match server.socket.accept() {
-                        Err(e) => { println!("Accept error: {}", e); return; },
+            let token = event.token();
+            let ready = event.readiness();
+
+            // when read
+            if ready.is_readable() {
+                println!("readable");
+                match token {
+                    // first connection
+                    SERVER => {
+                        let client_socket = match server.socket.accept() {
+                            Err(e) => { println!("Accept error: {}", e); return; },
                         Ok((sock, _)) => sock
-                    };
-                    println!("{:?}", client_socket);
-                    server.token_counter += 1;
-                    let new_token = Token(server.token_counter);
-                    server.clients.insert(new_token, WebSocketClient::new(client_socket));
+                        };
+                        println!("{:?}", client_socket);
+                        server.token_counter += 1;
+                        let new_token = Token(server.token_counter);
+                        server.clients.insert(new_token, WebSocketClient::new(client_socket));
 
-                    println!("{:?}", new_token);
+                        println!("{:?}", new_token);
 
-                    poll.register(
-                        &server.clients[&new_token].socket,
-                        new_token,
-                        Ready::readable(),
-                        PollOpt::edge() | PollOpt::oneshot()
-                    ).unwrap();
+                        poll.register(
+                            &server.clients[&new_token].socket,
+                            new_token,
+                            Ready::readable(),
+                            PollOpt::edge() | PollOpt::oneshot()
+                        ).unwrap();
+                    }
+                    // client per connection
+                    token => {
+                        let mut client = server.clients.get_mut(&token).unwrap();
+                        client.read();
+                        poll.reregister(
+                            &client.socket,
+                            token,
+                            client.interest,
+                            PollOpt::edge() | PollOpt::oneshot()
+                        ).unwrap();
+                    },
                 }
-                // client per connection
-                token => {
-                    let mut client = server.clients.get_mut(&token).unwrap();
-                    client.read();
-                    poll.reregister(
-                        &client.socket,
-                        token,
-                        client.interest,
-                        PollOpt::edge() | PollOpt::oneshot()
-                    ).unwrap();
-                },
+            }
+
+            // when write
+            if ready.is_writable() {
+                println!("writable");
+                let mut client = server.clients.get_mut(&token).unwrap();
+                client.write();
+                poll.reregister(
+                    &client.socket,
+                    token,
+                    client.interest,
+                    PollOpt::edge() | PollOpt::oneshot()
+                ).unwrap();
             }
         }
     }
